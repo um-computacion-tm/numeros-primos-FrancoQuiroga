7f96ef738d281d98ed39683ba55dd34851718644
FrancoQuiroga <fe.quiroga@um.edu.ar>
2024-03-30 17:30:20 -0300

    Rework in the is_prime function

--- isprime.py
+++ isprime.py
@@ -1,20 +1,36 @@
 import unittest
+import random
+"Miller-Rabin prime function"
 def is_prime(numero):
-    numerocheck = 9
-    result = False
-    if 0 < numero < 4:
-        result = True
-        return result
-    while numerocheck > 0:
-        divisioncheck = numero % numerocheck
-        if (numero % 2) == 0:
-            return False
-        if divisioncheck == 0:
-            result = False
+    if numero < 4:
+        return True
+    es_primo = False
+    cantdepruebas = 20
+    for i in range(cantdepruebas):
+        randomnum = random.randint(1,numero-1)
+        if (randomnum ** (numero-1) % numero) != 1 % numero:
+            es_primo = False
         else:
-            return True
-        numerocheck = numerocheck - 1
-    return result
+            es_primo = True
+            return es_primo
+    return es_primo
+"Prime Function that only works up to 90"
+#def is_prime(numero):
+#    numerocheck = 9
+#    result = False
+#    if 0 < numero < 4:
+#        result = True
+#        return result
+#    while numerocheck > 0:
+#        divisioncheck = numero % numerocheck
+#        if (numero % 2) == 0:
+#            return False
+#        if divisioncheck == 0:
+#            result = False
+#        else:
+#            return True
+#        numerocheck = numerocheck - 1
+#    return result
 class Testnumprimo(unittest.TestCase):
     def test_num1(self):
         resultado = is_prime(1)
@@ -38,6 +54,10 @@ class Testnumprimo(unittest.TestCase):
     def test_num97(self):
         resultado = is_prime(97)
         self.assertTrue(resultado)
+
+    def test_num95(self):
+        resultado = is_prime(97)
+        self.assertFalse(resultado)
 
     def test_num6113(self):
         resultado = is_prime(6113)
